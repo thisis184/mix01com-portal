a1902421b67eac66f91a47ee059de7f31beece9b
thisis184 <thisis184@gmail.com>
2022-05-13 18:07:07 +0800

    Upadte: index.vue
    Add logo.

--- pages/index.vue
+++ pages/index.vue
@@ -7,18 +7,18 @@
       <v-row align="center" justify="center" style="color: black">
         <v-spacer />
         <v-col cols="6">
-          <v-row justify="center"><h1>mix01 Studio 中文</h1></v-row>
-          <v-row>
-            <v-col v-for="n in 3" :key="n" cols="4">
-              <v-row justify="center">
-                <h3>service title</h3>
-              </v-row>
-            </v-col>
+          <v-row justify="center">
+            <img class="logo" :src="require('~/assets/images/logo.png')" />
+          </v-row>
+          <v-row justify="center">
+            <h1>MIX01 STUDIO</h1>
           </v-row>
           <v-row>
             <v-divider />
           </v-row>
-          <v-row justify="center"> slogen </v-row>
+          <v-row justify="center">
+            <h4>數位設計 / 全端開發</h4>
+          </v-row>
         </v-col>
         <v-spacer />
       </v-row>
